1634573f7383c41d6a34cfa0d6459cb41f21f820
Hung <daminhnguyenhung@gmail.com>
2023-04-20 20:33:59 +0700

    base

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import * as cheerio from "cheerio";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [url, setUrl] = useState("");
+    const [links, setLinks] = useState([]);
+
+    async function getHtml(url) {
+        const { data: html } = await axios.get(url);
+
+        return html;
+    }
+
+    function getLink(html) {
+        let links = [];
+
+        const $ = cheerio.load(html);
+        $("a").each((index, item) => {
+            links.push(item);
+        });
+
+        return links;
+    }
+
+    function hanldeGetLink() {
+        const html = getHtml(url);
+        const a_elements = getLink(html);
+
+        const result = a_elements.reduce((acc, curr, index) => {
+            if (curr?.attribs?.href && curr?.attribs?.title) {
+                return [
+                    ...acc,
+                    { title: curr?.attribs?.title, href: curr?.attribs?.href },
+                ];
+            }
+
+            return acc;
+        }, []);
+
+        setLinks(result);
+    }
+
+    return (
+        <div className="wrapper">
+            <input
+                type="text"
+                placeholder="Enter url"
+                onChange={(e) => setUrl(e.target.value)}
+                value={url}
+            />
+
+            <button onClick={hanldeGetLink}>Get</button>
+
+            <ul>
+                {links.length > 0 &&
+                    links.map((link, index) => {
+                        return (
+                            <li key={index}>
+                                <a href={link.href}>{link.title}</a>
+                                <span>{link.href}</span>
+                            </li>
+                        );
+                    })}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
